7fa0802d815a7833cc15e6939bb9728e1f67623c
adung1211 <dungphamanh2003@gmail.com>
2024-10-23 03:05:19 +0700

    update

--- run.py
+++ run.py
@@ -41,60 +41,63 @@ def process_image():
 
     # Open and modify the card image
     with Image.open(input_image_path) as img:
-        
         draw = ImageDraw.Draw(img)
 
         position = (560, 235)            
         font_size =  28               
         text_color = (19, 97, 180)
         font = ImageFont.truetype("./fonts/cocofy.ttf", font_size)
-            
+
+        indent_len = 20
         max_x = 700  # Maximum width of the text area
-        words = longText.split(' ')
-        current_line = []
-        word_ident = True
+        lines = [line for line in longText.split('\r\n') if line.strip()]  # Split text into lines based on newlines
         
-        for word in words:
-            # Add word to current line
-            current_line.append(word)
-            # Join current line into a single string
-            line_text = ' '.join(current_line)
-            # Measure the bounding box of the current line
-            line_bbox = draw.textbbox((0, 0), line_text, font=font)
-            line_x = line_bbox[2]
+        for line in lines:
+            words = line.split(' ')  # Split each line into words
+            current_line = []
+            word_ident = True
             
-            # If the line is too wide, finalize the previous line and start a new one
-            if line_x + 30 > max_x:
-                # Remove the last word from the current line
-                current_line.pop()
-                # Draw the previous line justified
-                if current_line:
-                    total_width = draw.textbbox((0, 0), ' '.join(current_line), font=font)[2]
-                    spaces_needed = len(current_line) - 1
-                    if spaces_needed > 0:
-                        total_space = max_x - total_width
-                        x = position[0]
-                        if (word_ident == True):
-                                x += 20
-                                word_ident = False
-                                total_space -=20
-
-                        space_between_words = total_space / spaces_needed
-                        for i, word2 in enumerate(current_line):
-
-                            draw.text((x, position[1]), word2, font=font, fill=text_color)
-
-                            x += draw.textbbox((0, 0), word2, font=font)[2]
-                            if i < spaces_needed:  # Avoid adding space after the last word
-                                x += space_between_words
+            for word in words:
+                # Add word to current line
+                current_line.append(word)
+                # Join current line into a single string
+                line_text = ' '.join(current_line)
+                # Measure the bounding box of the current line
+                line_bbox = draw.textbbox((0, 0), line_text, font=font)
+                line_x = line_bbox[2]
                 
-                # Move to the next line
-                position = (position[0], position[1] + 40)
-                current_line = [word]  # Start new line with the last word
+                # If the line is too wide, finalize the previous line and start a new one
+                if line_x + 30 > max_x:
+                    # Remove the last word from the current line
+                    current_line.pop()
+                    # Draw the previous line justified
+                    if current_line:
+                        total_width = draw.textbbox((0, 0), ' '.join(current_line), font=font)[2]
+                        spaces_needed = len(current_line) - 1
+                        if spaces_needed > 0:
+                            total_space = max_x - total_width
+                            x = position[0]
+                            if word_ident:
+                                x += indent_len
+                                word_ident = False
+                                total_space -= indent_len
 
-        # Draw the last line left-aligned
-        if current_line:
-            draw.text(position, ' '.join(current_line), font=font, fill=text_color)
+                            space_between_words = total_space / spaces_needed
+                            for i, word2 in enumerate(current_line):
+                                draw.text((x, position[1]), word2, font=font, fill=text_color)
+                                x += draw.textbbox((0, 0), word2, font=font)[2]
+                                if i < spaces_needed:  # Avoid adding space after the last word
+                                    x += space_between_words
+
+                    # Move to the next line
+                    position = (position[0], position[1] + 40)  # Adjust vertical position for the next line
+                    current_line = [word]  # Start new line with the last word
+
+            # Draw the last line left-aligned if there are words remaining
+            if current_line:
+                if (word_ident == True): draw.text((position[0] + indent_len, position[1]), ' '.join(current_line), font=font, fill=text_color)
+                else: draw.text(position, ' '.join(current_line), font=font, fill=text_color)
+                position = (position[0], position[1] + 40)  # Move to the next line after drawing the last line
 
         #################################################################################################################
         align_right = 1200  
